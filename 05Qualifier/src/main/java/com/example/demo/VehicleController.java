d5dc778af19dff34900f4694cda1b3994e28cd42
spacky12 <spacky12@yahoo.com>
2025-08-28 23:24:30 +0530

    Qualifier1

---
+++ 05Qualifier/src/main/java/com/example/demo/VehicleController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VehicleController {
+    Vehicle v1;
+
+    @Autowired
+    VehicleController(@Qualifier("car") Vehicle v1)
+    {
+        this.v1=v1;
+    }
+    @GetMapping("/speed")
+    String getTopSpeed()
+    {
+        return v1.getTopSpeed();
+    }
+}
